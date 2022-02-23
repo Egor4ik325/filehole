bfc66ad7d9ed4573bc95d0c34814fb8582018eb2
Egor4ik325 <zorinegor325@gmail.com>
2022-02-23 23:59:21 +0300

    Add uppy file uploader

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,10 +1,25 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
+
+import Draggable from "react-draggable";
+import Uppy from "@uppy/core";
+import Tus from "@uppy/tus";
+import { useUppy, DragDrop } from "@uppy/react";
+
 import logo from "./assets/logo.svg";
 import { listFiles, uploadFile } from "./client";
 
 const App = () => {
   const [files, setFiles] = useState(undefined);
+
+  // Uppy file uploader instance
+  const uppy = useUppy(() => {
+    return new Uppy({
+      restrictions: { maxNumberOfFiles: 1 },
+      autoProceed: true,
+    }).use(Tus, {
+      endpoint: "https://tusd.tusdemo.net/files",
+    });
+  });
 
   const fetchFiles = async () => {
     try {
@@ -24,14 +39,35 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="app">
+      <div className="header"></div>
       Filehole <img src={logo} alt="logo" width={200} />
+      <DragDrop
+        uppy={uppy}
+        width="100%"
+        height="100%"
+        note="Images up to 200×200px"
+        locale={{
+          strings: {
+            // Text to show on the droppable area.
+            // `%{browse}` is replaced with a link that opens the system file selection dialog.
+            dropHereOr: "Drop here or %{browse}",
+            // Used as the label for the link that opens the system file selection dialog.
+            browse: "browse",
+          },
+        }}
+      />
       {files !== undefined &&
         files.map((file) => (
-          <div key={file.id}>
-            Name: {file.name}
-            <img src={file.url} alt="file" width="100" />
-          </div>
+          <Draggable key={file.id}>
+            <div>
+              Name: {file.name}
+              <img src={file.url} alt="file" width="100" />
+              <a href={file.url} download={file.name}>
+                Download
+              </a>
+            </div>
+          </Draggable>
         ))}
       <form>
         <input
@@ -41,6 +77,9 @@ const App = () => {
           onChange={handleImageUploadChange}
         />
       </form>
+      <Draggable>
+        <div>Hello, World!</div>
+      </Draggable>
     </div>
   );
 };
